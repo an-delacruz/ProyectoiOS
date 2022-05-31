249b2596520725e1e21dda0bc5d86cb409285a4e
JCabrera13 <L18100155@nlaredo.tecnm.mx>
2022-05-30 23:40:16 -0500

    viewConfiguration Pantalla configuration

---
+++ ProyectoFinal_IOS/Controllers/viewConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  viewConfiguration.swift
+//  ProyectoFinal_IOS
+//
+//  Created by Karla Marquez on 5/30/22.
+//
+
+import Foundation
+import UIKit
+
+class viewConfiguration:UIViewController{
+    
+}
