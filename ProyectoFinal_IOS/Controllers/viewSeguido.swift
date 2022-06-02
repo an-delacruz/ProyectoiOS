f9f13a245b581f3f1aff46c0f6b8913ad2f48d62
JCabrera13 <L18100155@nlaredo.tecnm.mx>
2022-06-01 20:04:50 -0500

    table view seguidos modificacion colores pantalla login, sign out, publicaciones

---
+++ ProyectoFinal_IOS/Controllers/viewSeguido.swift
@@ -0,0 +1,13 @@
+//
+//  viewSeguido.swift
+//  ProyectoFinal_IOS
+//
+//  Created by Karla Marquez on 6/1/22.
+//
+
+import Foundation
+import UIKit
+
+class viewSeguido:UITableViewController{
+    
+}
